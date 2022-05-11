c2143f9dbed84a2cee199e56023903c3d688f52d
Lemont Gaétan <gaetanl2004@gmail.com>
2022-05-11 12:09:53 +0200

    Fin de séance

---
+++ src/Entity/Artist.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class Artist
+{
+private int $id;
+private string $name;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
