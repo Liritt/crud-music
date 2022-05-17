89ab7f1c7a06b71eaf29228bdc3f57934c2abd2c
Lemont Gaétan <gaetanl2004@gmail.com>
2022-05-17 11:15:29 +0200

    Ajout à l'index git

--- src/Entity/Collection/ArtistCollection.php
+++ src/Entity/Collection/ArtistCollection.php
@@ -1,11 +1,33 @@
 <?php
+
 declare(strict_types=1);
 
-namespace Collection;
+namespace Entity\Collection;
+
+use Entity\Artist;
+use mysql_xdevapi\SqlStatement;
+use Database\MyPdo;
+use PDO;
 
 class ArtistCollection
 {
-    public function findAll(){
-        
+    /**
+     * retourne un tableau contenant tous les artistes triés par ordre alphabétique
+     * @return Artist[]
+     */
+    public function findAll(): array
+    {
+        $tab = [];
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+                SELECT name
+                FROM artist
+                ORDER BY name
+            SQL
+        );
+        while (($line = $stmt->fetchAll(PDO::FETCH_CLASS, PDO::FETCH_PROPS_LATE)) !== false) {
+            $tab[]=$line;
+        }
+        return $tab;
     }
 }
